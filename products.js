311be2f2448406f397aba5b10c5e8b166663dbcd
vigneshpyc <145823761+vigneshpyc@users.noreply.github.com>
2024-03-31 21:02:39 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,11 @@
+function products(){
+    window.open("file:///G:/HTML%20Learning/FarmerConnect/products.html");
+
+}
+function farmer(){
+    window.open("file:///G:/HTML%20Learning/FarmerConnect/farmers.html")
+
+}
+function govtscheme(){
+    window.open("https://farmer.gov.in/")
+}
